7dfd3986c1873c94517c045f3de3e251bfa33c53
Rikki Katz <katz.rikki@gmail.com>
2014-09-16 13:38:12 -0400

    Added links for rooms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def room1
+  end
+
+  def room2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Website::Application.routes.draw do
   devise_for :users
   root "pages#home"
+	
+	#links for each meeting room
+	get "room1" => "pages#room1"
+	get "room2" => "pages#room2"	
+
 end
