dec28cb8e06602f229223258e0b0f81ecf491b49
Rikki Katz <katz.rikki@gmail.com>
2014-11-07 12:03:06 -0500

    added new reservation path to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Website::Application.routes.draw do
 	resources :reservations
-
-	resources :rooms
-
 	devise_for :users
+	
 	root "pages#home"
-
-	#links for each meeting room
-	get "room1" => "pages#room1"
-	get "room2" => "pages#room2"
 
 end
