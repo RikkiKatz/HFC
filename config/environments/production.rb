6dff8e39a0632dad864dcf6e07002f8b8f759a67
Rikki Katz <katz.rikki@gmail.com>
2014-12-11 13:59:50 -0500

    load timepicker before turbolinks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Website::Application.configure do
 
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local  = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = true
   config.assets.js_compressor = :uglifier
@@ -10,7 +10,6 @@ Website::Application.configure do
   config.assets.digest = true
   config.assets.version = '1.0'
   config.assets.precompile += ['jquery.js', 'jquery_ujs.js']
-  config.assets.precompile += %w( jquery-timepicker-jt.js )
   config.log_level = :info
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
